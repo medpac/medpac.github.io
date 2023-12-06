36cadd5d1e0782b63e5d326a94fc128ba6c7f8d7
chekinfotech <99319959+chekinfotech@users.noreply.github.com>
2023-12-05 18:57:14 -0800

    v1

--- script.js
+++ script.js
@@ -256,3 +256,31 @@ function submitAttestation() {
     document.getElementById('display-auth-code').textContent = document.getElementById('code').textContent;
 }
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  document.getElementById('youtube').contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById('youtube').contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+  }
+}
